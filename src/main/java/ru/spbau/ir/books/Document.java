e8a9e83e1af9a23d230ccec147033d7a2c0b3cb6
Angelika <anzhelikamukhina@yandex.ru>
2017-10-19 23:32:10 +0300

    parse method of Document class is implemented

--- src/main/java/ru/spbau/ir/books/Document.java
+++ src/main/java/ru/spbau/ir/books/Document.java
@@ -1,9 +1,15 @@
 package ru.spbau.ir.books;
 
 
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Document {
@@ -12,9 +18,25 @@ public class Document {
     Document(File htmlPage) {
         this.htmlPage = htmlPage;
     }
-    
+
     public List<URL> parse() {
-        return null;
+        org.jsoup.nodes.Document doc = null;
+        try {
+            doc = Jsoup.parse(htmlPage, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Elements links = doc.select("a[href");
+        List<String> urlsStrings = links.eachAttr("abs:href");
+        List<URL> urls = new ArrayList<>();
+        for (String urlString : urlsStrings) {
+            try {
+                urls.add(new URL(urlString));
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+        return urls;
     }
 
     public void store(Path path) {
