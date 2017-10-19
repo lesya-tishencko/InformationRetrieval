86659b2461e86ae5166444c64623f017a69c3212
lesya-tishencko <lesya.tishencko2012@yandex.ru>
2017-10-19 22:12:12 +0300

    Added crawler logic

--- src/main/java/ru/spbau/ir/books/Crawler.java
+++ src/main/java/ru/spbau/ir/books/Crawler.java
@@ -4,13 +4,11 @@ import javafx.scene.shape.Path;
 
 import java.net.URL;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class Crawler {
 
-    private Map<URL, Document> maps = new HashMap<URL, Document>();
+    private Map<URL, Document> processedUrls = new HashMap<URL, Document>();
     private Path pathForStoring;
 
     public void crawlerThread(Frontier frontier) {
@@ -18,11 +16,11 @@ public class Crawler {
             WebsiteAndUrl siteAndUrl = frontier.nextSite();
             Website site = siteAndUrl.site;
             URL url = siteAndUrl.url;
-            if (!maps.containsKey(url)) {
+            if (!processedUrls.containsKey(url)) {
                 if (site.permitsCrawl(url)) {
                     Document document = site.getDocument();
                     document.store(pathForStoring);
-                    maps.put(url, document);
+                    processedUrls.put(url, document);
                     frontier.addUrl(document.parse());
                 }
             }
