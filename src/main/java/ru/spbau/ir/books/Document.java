fb623b1af430a375b3cf528efea1528fdecd8cc9
Angelika <anzhelikamukhina@yandex.ru>
2017-10-19 23:06:23 +0300

    changes in Document's interface

--- src/main/java/ru/spbau/ir/books/Document.java
+++ src/main/java/ru/spbau/ir/books/Document.java
@@ -1,11 +1,18 @@
 package ru.spbau.ir.books;
 
 
+import java.io.File;
 import java.net.URL;
 import java.nio.file.Path;
 import java.util.List;
 
 public class Document {
+    private File htmlPage;
+
+    Document(File htmlPage) {
+        this.htmlPage = htmlPage;
+    }
+    
     public List<URL> parse() {
         return null;
     }
