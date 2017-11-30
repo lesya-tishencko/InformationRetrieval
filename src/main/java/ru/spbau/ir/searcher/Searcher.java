32a4eb4d308aeaef5e28293e78a839914be3e77d
lesya-tishencko <lesya.tishencko2012@yandex.ru>
2017-12-01 02:36:55 +0300

    Added getSimilar implementation

--- src/main/java/ru/spbau/ir/searcher/Searcher.java
+++ src/main/java/ru/spbau/ir/searcher/Searcher.java
@@ -18,8 +18,10 @@ public class Searcher {
     private final double averageLength;
     private final double k1 = 2.0;
     private final double b = 0.75;
+    private final DBHandler dbHandler;
 
     public Searcher(DBHandler dbHandler) {
+        this.dbHandler = dbHandler;
         documentsLength = dbHandler.getDocumentsLength();
         N = documentsLength.size();
         averageLength = documentsLength.values().stream().reduce(0, (acc, next) -> acc + next) / N;
@@ -63,8 +65,9 @@ public class Searcher {
         return frequency.get(id);
     }
 
-    public PriorityQueue<DocumentBlock> getSimilar(int id) {
-        return null;
+    public PriorityQueue<BM25Ranker> getSimilar(int id) {
+        String annotations = dbHandler.getDescription(id);
+        return searchByPlot(annotations);
     }
 
     public class BM25Ranker implements Comparable<BM25Ranker> {
