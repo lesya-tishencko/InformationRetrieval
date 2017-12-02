993bdd1c22639dca99eff852bdc334297360320a
Angelika <anzhelikamukhina@yandex.ru>
2017-12-02 14:30:11 +0300

    changes

--- src/main/java/ru/spbau/ir/ui/UI.java
+++ src/main/java/ru/spbau/ir/ui/UI.java
@@ -8,6 +8,7 @@ import ru.spbau.ir.utils.Book;
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.PriorityQueue;
 
 public class UI {
@@ -15,6 +16,9 @@ public class UI {
     private static int windowWidth;
     private static int horMargin;
     private static int verMargin;
+    private static int standardElementHeight;
+    private static int standardElementWidth;
+
     public static void createAndShowUI() {
         DBHandler dbHandler = new DBHandler();
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
@@ -22,6 +26,8 @@ public class UI {
         windowHeight = (int)(screen.height * 0.75);
         horMargin = windowWidth / 10;
         verMargin = windowHeight / 20;
+        standardElementHeight = windowHeight / 25;
+        standardElementWidth = windowWidth - 2 * horMargin;
 
         JFrame jframe = new JFrame("Поиск книг");
         jframe.setLayout(null);
@@ -68,9 +74,10 @@ public class UI {
         while (!list.isEmpty()) {
             Searcher.BM25Ranker bookId = list.poll();
             Book book = dbHandler.getBook(bookId.getDocumentId());
-            JButton bookButton = new JButton(book.getAuthor() + " " + "\"" + book.getName() + "\"");
+            JButton bookButton = new JButton(book.getAuthor() + " " + " \"" +
+                    book.getName() + "\" (" + book.getSite() + ")");
             bookButton.addActionListener(actionEvent1 -> {
-                showBookInfo(dbHandler, screen, jframe, searcher, bookId, book);
+                showBookInfo(dbHandler, screen, searcher, bookId, book);
             });
             jframe.setLayout(null);
             jframe.add(bookButton);
@@ -82,6 +89,24 @@ public class UI {
             );
             bookButton.setSize(windowWidth - 2 * horMargin - searchButtonWidth - 2,
                     searchButtonHeight);
+            JProgressBar pbar = new JProgressBar();
+            pbar.setMinimum(0);
+            pbar.setMaximum(100);
+            pbar.setBounds(windowWidth - searchButtonWidth - horMargin,
+                    verMargin + searchButtonHeight * (2 + bookNo),
+                    searchButtonWidth,
+                    searchButtonHeight);
+            OptionalDouble averageScoreOpt = dbHandler
+                    .getBooksReviewsSentimScores(bookId
+                            .getDocumentId())
+                    .stream()
+                    .mapToDouble(a -> a)
+                    .average();
+            if (averageScoreOpt.isPresent()) {
+                pbar.setValue((int)(averageScoreOpt.getAsDouble() * 100));
+            }
+
+            jframe.add(pbar);
             jframe.invalidate();
             jframe.repaint();
             bookNo++;
@@ -90,18 +115,21 @@ public class UI {
 
     private static void showBookInfo(DBHandler dbHandler,
                                      Dimension screen,
-                                     JFrame jframe,
                                      Searcher searcher,
                                      Searcher.BM25Ranker bookId,
                                      Book book) {
-        JFrame bookFrame = new JFrame("Book information");
-        jframe.setLayout(null);
-        JLabel author = new JLabel(book.getAuthor());
-        JLabel name = new JLabel(book.getName());
-        JLabel description = new JLabel(book.getDescription());
-        author.setBounds(0, 0, 200, 100);
-        name.setBounds(0, 100, 200, 100);
-        description.setBounds(0, 200, 200, 100);
+        JFrame bookFrame = new JFrame("Информация о книге");
+        JLabel author = new JLabel();
+        author.setText(book.getAuthor());
+        author.setBounds(horMargin, verMargin, windowWidth - 2 * horMargin, standardElementHeight);
+        JLabel name = new JLabel();
+        name.setText("\"" + book.getName() + "\"");
+        name.setBounds(horMargin, verMargin + standardElementHeight + 1, windowWidth - 2 * horMargin, standardElementHeight);
+        JLabel description = new JLabel("<html>" + book.getDescription() + "</html>");
+        description.setBounds(horMargin,
+                verMargin + standardElementHeight * 2 + 1,
+                windowWidth - 2 * horMargin,
+                standardElementHeight * 5);
         bookFrame.add(author);
         bookFrame.add(name);
         bookFrame.add(description);
@@ -109,14 +137,19 @@ public class UI {
                 = searcher.getSimilar(bookId.getDocumentId());
         RankerByScore ranker = new RankerByScore(dbHandler);
         List<Integer> docsIds = ranker.rankByScore(docsList);
+        int docNo = 0;
         for (Integer docId : docsIds) {
             Book currBook = dbHandler.getBook(docId);
             JLabel bookLabel = new JLabel(
-                    currBook.getAuthor() + " " + "\"" + currBook.getName());
-            bookLabel.setBounds(0, 300, 200, 100);
+                    currBook.getAuthor() + " " + "\"" + currBook.getName() + "\"");
+            bookLabel.setBounds(horMargin,
+                    verMargin + standardElementHeight * (8 + docNo) + docNo + 1,
+                    windowWidth - 2 * horMargin,
+                    standardElementHeight);
             bookFrame.add(bookLabel);
+            docNo++;
         }
-        bookFrame.setSize(windowWidth,windowHeight);
+        bookFrame.setSize(windowWidth, windowHeight);
         bookFrame.setBounds((screen.width - windowWidth) / 2,
                 (screen.height - windowHeight) / 2,
                 windowWidth,
